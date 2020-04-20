ca82ab0ee1e2396b2d2322cfb22b5e028f84455c
Dan Dobrin <dan.dobrin@gmail.com>
2020-04-20 14:15:11 -0400

    updated cnb building

---
+++ build-paketo.sh
@@ -0,0 +1,5 @@
+pack build triathlonguy/todos-api --publish --path . --builder cloudfoundry/cnb:bionic --env BP_BUILT_MODULE=todos-api --env BP_BUILD_ARGUMENTS="-Dmaven.test.skip=false package -pl todos-api -am"
+pack build triathlonguy/todos-edge --publish --path . --builder cloudfoundry/cnb:bionic --env BP_BUILT_MODULE=todos-edge --env BP_BUILD_ARGUMENTS="-Dmaven.test.skip=false package -pl todos-edge -am"
+pack build triathlonguy/todos-webui --publish --path . --builder cloudfoundry/cnb:bionic --env BP_BUILT_MODULE=todos-webui --env BP_BUILD_ARGUMENTS="-Dmaven.test.skip=false package -pl todos-webui -am"
+pack build triathlonguy/todos-mysql --publish --path . --builder cloudfoundry/cnb:bionic --env BP_BUILT_MODULE=todos-mysql --env BP_BUILD_ARGUMENTS="-Dmaven.test.skip=false package -pl todos-mysql -am"
+pack build triathlonguy/todos-redis --publish --path . --builder cloudfoundry/cnb:bionic --env BP_BUILT_MODULE=todos-redis --env BP_BUILD_ARGUMENTS="-Dmaven.test.skip=false package -pl todos-redis -am"
